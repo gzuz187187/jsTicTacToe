8109055913a8c85c9dd4167e606ef088603fd3e6
gzuz187187 <thomas2399@gmx.at>
2021-12-22 23:45:50 +0100

    completed functionality

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const sourceX = "./images/x.png";
-const sourceO = "./images/o2.png";
-let actSource = sourceX;
-let pIsX = true;
+const sourceX = "./images/x_grey.png";
+const sourceO = "./images/o3.png";
+
+let actSource = "";
+let pIsX = "";
 let gameStatus = "";    // either w (win), d (draw) or l (lose); X is the player that is used to calculate the game status
+
+let rnd = Math.floor(Math.random() * 2);
+
+if (rnd == 0) {
+    actSource = sourceX;
+    pIsX = true;
+} else {
+    actSource = sourceO;
+    pIsX = false;
+}
 
 let cell1 = document.querySelector("[data-cell1]");
 let imageCell1 = document.querySelector("[image-cell1]");
@@ -23,10 +34,6 @@ let imageCell8 = document.querySelector("[image-cell8]");
 let cell9 = document.querySelector("[data-cell9]");
 let imageCell9 = document.querySelector("[image-cell9]");
 
-function initField() {
-    ;
-}
-
 function changePlayer() {     // checks active source and active player
     if (pIsX == true) {
         pIsX = false;
@@ -39,7 +46,7 @@ function changePlayer() {     // checks active source and active player
 
 function checkForGameEnd() {
     // cell1, cell2, cell3
-    if (imageCell1.src == imageCell2.src == imageCell3.src && Boolean(imageCell1.src) != false && Boolean(imageCell2.src) != false && Boolean(imageCell3.src) != false) {
+    if (imageCell1.src == imageCell2.src && imageCell2.src == imageCell3.src && imageCell1.src == imageCell3.src && Boolean(imageCell1.src) != false && Boolean(imageCell2.src) != false && Boolean(imageCell3.src) != false) {
         if (pIsX == true) {
             gameStatus = "w";
         } else {
@@ -48,7 +55,7 @@ function checkForGameEnd() {
     }
 
     // cell4, cell5, cell6
-    if (imageCell4.src == imageCell5.src == imageCell6.src && Boolean(imageCell4.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell6.src) != false) {
+    if (imageCell4.src == imageCell5.src && imageCell5.src == imageCell6.src && imageCell4.src == imageCell6.src && Boolean(imageCell4.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell6.src) != false) {
         if (pIsX == true) {
             gameStatus = "w";
         } else {
@@ -57,7 +64,34 @@ function checkForGameEnd() {
     }
 
     // cell7, cell8, cell9
-    if (imageCell7.src == imageCell8.src == imageCell9.src && Boolean(imageCell7.src) != false && Boolean(imageCell8.src) != false && Boolean(imageCell9.src) != false) {
+    if (imageCell7.src == imageCell8.src && imageCell8.src == imageCell9.src && imageCell7.src == imageCell9.src && Boolean(imageCell7.src) != false && Boolean(imageCell8.src) != false && Boolean(imageCell9.src) != false) {
+        if (pIsX == true) {
+            gameStatus = "w";
+        } else {
+            gameStatus = "l";
+        }
+    }
+
+    // cell1, cell4, cell7
+    if (imageCell1.src == imageCell4.src && imageCell4.src == imageCell7.src && imageCell1.src == imageCell7.src && Boolean(imageCell1.src) != false && Boolean(imageCell4.src) != false && Boolean(imageCell7.src) != false) {
+        if (pIsX == true) {
+            gameStatus = "w";
+        } else {
+            gameStatus = "l";
+        }
+    }
+
+    // cell2, cell5, cell8
+    if (imageCell2.src == imageCell5.src && imageCell5.src == imageCell8.src && imageCell2.src == imageCell8.src && Boolean(imageCell2.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell8.src) != false) {
+        if (pIsX == true) {
+            gameStatus = "w";
+        } else {
+            gameStatus = "l";
+        }
+    }
+
+    // cell3, cell6, cell9
+    if (imageCell3.src == imageCell6.src && imageCell6.src == imageCell9.src && imageCell3.src == imageCell9.src && Boolean(imageCell3.src) != false && Boolean(imageCell6.src) != false && Boolean(imageCell9.src) != false) {
         if (pIsX == true) {
             gameStatus = "w";
         } else {
@@ -66,7 +100,7 @@ function checkForGameEnd() {
     }
 
     // cell1, cell5, cell9
-    if (imageCell1.src == imageCell5.src == imageCell9.src && Boolean(imageCell1.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell9.src) != false) {
+    if (imageCell1.src == imageCell5.src && imageCell5.src == imageCell9.src && imageCell1.src == imageCell9.src && Boolean(imageCell1.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell9.src) != false) {
         if (pIsX == true) {
             gameStatus = "w";
         } else {
@@ -75,7 +109,7 @@ function checkForGameEnd() {
     }
 
     // cell3, cell5, cell7
-    if (imageCell3.src == imageCell5.src == imageCell7.src && Boolean(imageCell3.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell7.src) != false) {
+    if (imageCell3.src == imageCell5.src && imageCell5.src == imageCell7.src && imageCell3.src == imageCell7.src && Boolean(imageCell3.src) != false && Boolean(imageCell5.src) != false && Boolean(imageCell7.src) != false) {
         if (pIsX == true) {
             gameStatus = "w";
         } else {
@@ -96,6 +130,10 @@ function checkGameStatus() {
         alert("Player O wins!");
     } else if (gameStatus == "d") {
         alert("It's a draw!");
+    }
+
+    if (gameStatus) {
+        window.location.href = "./landingPage.html";
     }
 }
 
